53a48753377ae139cf67252caa57f837b269b64d
Colin J. Mills <colin.j.mills96@gmail.com>
2018-06-28 19:18:09 +0000

    Fixup usage

--- util.php
+++ util.php
@@ -1,10 +1,23 @@
 <?php
 
+// callCallback($name, $params)
+//
+// A simple utility that helps make closures easier to call.
+//
+// $name: The function we are calling.
+// $params: The parameters.
+//
+// NOTE: I wrote this because I dont like how youo have to pass a string to call_user_func.
 function callCallback($name, $params)
 {
     $fn = $name;
     $fn($params);
 }
+
+// dd($value)
+//
+// A simple clone f Laravel and Symphony's dd function.
+//
 function dd($value)
 {
     var_dump($value);
@@ -13,6 +26,6 @@ function dd($value)
 
 function usage()
 {
-    printf("Usage: php main.php [-[u]rl -[o]utput-file\n");
+    printf("Usage: php main.php [-[u]rl -[o]utput-file -[c]hunk-size -[s]egment-size\n");
     exit();
 }
